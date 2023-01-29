a8fe668acba6b7fbb302f9de17f094b53126e5fe
QueenTesa Fuggett <qfuggett@gmail.com>
2023-01-29 00:23:07 -0500

    add app to settings and create models

---
+++ quotes/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Author(models.Model):
+    "Author model"
+
+    name = models.CharField(max_length=55)
+
+    def __repr__(self):
+        return f'Author: %s %s' % (self.name)
+
+
+class AuthorPhoto(models.Model):
+    "AuthorPhoto model"
+
+    photo = models.CharField(max_length=254)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Quote(models.Model):
+    "Quote model"
+
+    description = models.CharField(max_length=25)
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL)
+    favorite = models.BooleanField(default=False)
+
+    def __repr__(self):
+        return f'Quote: %s %s' % (self.description, self.author)
