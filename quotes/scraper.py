a432abd7cbdec38434417b2937bed1b485367689
QueenTesa Fuggett <qfuggett@gmail.com>
2023-02-11 19:38:50 +0100

    update scraper

--- quotes/scraper.py
+++ quotes/scraper.py
@@ -1,12 +1,21 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://becentsational.com/best-rap-quotes/"
-response = requests.get(URL)
+def scrape_quote() -> list:
+    URL = "https://becentsational.com/best-rap-quotes/"
+    response = requests.get(URL)
 
-soup = BeautifulSoup(response.content, "html.parser")
-quotes = soup.find_all("blockquote", class_="wp-block-quote")
-num = 1
-for quote in quotes:
-    num = num + 1
-    print("QUOTE", num, quote)
+    soup = BeautifulSoup(response.content, "html.parser")
+    quotes = soup.find_all("blockquote", class_="wp-block-quote")
+    num = 0
+
+    for quote in quotes:
+        num = num + 1
+        split = quote.text
+        author_split = re.split('[—―–]', split)
+        author = author_split[-1]
+        quote = author_split[0]
+        print(num, "By", author, ":", quote)
+
+        return [author, quote]
